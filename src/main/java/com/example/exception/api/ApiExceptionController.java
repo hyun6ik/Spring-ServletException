c2c3e395d0886468af21a0285229a4992efe3b9b
hyun6ik <fkanwm@naver.com>
2021-08-17 21:54:10 +0900

    [APIException] Api 예외처리 - 스프링부트가 제공하는 ExceptionResolver1

--- src/main/java/com/example/exception/api/ApiExceptionController.java
+++ src/main/java/com/example/exception/api/ApiExceptionController.java
@@ -1,11 +1,14 @@
 package com.example.exception.api;
 
 import com.example.exception.api.dto.MemberDto;
+import com.example.exception.exception.BadRequestException;
 import com.example.exception.exception.UserException;
 import org.apache.catalina.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ApiExceptionController {
@@ -25,5 +28,15 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
+    }
+
+    @GetMapping("/api/response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 }
